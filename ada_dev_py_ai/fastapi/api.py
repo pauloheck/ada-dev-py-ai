957e3f0b9c3891c7703ccf592405a0eaa1390703
pauloheck <pauloheck@gmail.com>
2024-11-23 00:30:13 -0300

    refactor: Remove unused OpenAI API integration and clean up code

--- ada_dev_py_ai/fastapi/api.py
+++ ada_dev_py_ai/fastapi/api.py
@@ -1,7 +1,5 @@
-import openai
-from config_openai import OPENAI_API_KEY, OPENAI_MODEL
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 from ada_dev_py_ai.crewai.create_story_ai import create_story_ai
 
@@ -18,8 +16,7 @@ class storyModel(BaseModel):
     criterios_aceitacao: str
 
 
-class PromptModel(BaseModel):
-    prompt: str = Field(..., example="Once upon a time...")
+app = FastAPI()
 
 
 @app.post('/story/create')
@@ -28,8 +25,6 @@ def create_story(input: requesitoModel):
     Create a story for the developer, by the user input requirements.
     Input the row requirements and the story will be created.
     """
-    try:
-    """
     print(f'input: {input}')
     return create_story_ai(input.input)
 
@@ -37,22 +32,3 @@ def create_story(input: requesitoModel):
 @app.get('/')
 def root_start():
     return {'message': 'Hello, world!'}
-
-
-# Inicializar o cliente OpenAI
-openai.api_key = OPENAI_API_KEY
-
-
-@app.post('/create-story')
-def story(input: PromptModel):
-    """
-    Gera uma história baseada no prompt fornecido usando a API da OpenAI.
-    """
-    prompt = input.prompt
-    """
-    try:
-        response = openai.Completion.create(engine=OPENAI_MODEL, prompt=prompt, max_tokens=150)
-        story = response.choices[0].text.strip()
-        return {'story': story}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
