b95b28a29906541d279a9346937397595a5b73f1
pauloheck <pauloheck@gmail.com>
2024-11-24 00:57:59 -0300

    feat: Update create_test_ai to use agent_test and task_create_test

--- ada_dev_py_ai/crewai/crew.py
+++ ada_dev_py_ai/crewai/crew.py
@@ -1,16 +1,17 @@
 from crewai import Crew
 
-from ada_dev_py_ai.crewai.agents import agent_po
-from ada_dev_py_ai.crewai.tasks import task_create_story
+from ada_dev_py_ai.crewai.agents import agent_po, agent_test
+from ada_dev_py_ai.crewai.tasks import task_create_story, task_create_test
 
 
 def create_test_ai(input: str):
-    crew = Crew(agents=[agent_po(input)], tasks=[task_create_story(input)], memory=True, verbose=True)
+    crew = Crew(agents=[agent_test(input)], tasks=[task_create_test(input)], memory=True, verbose=True)
 
     result = crew.kickoff()
     print(result.json_dict)
 
     return {'status': 'success', 'data': result.json_dict}
+
 
 def create_story_ai(input: str):
     crew = Crew(agents=[agent_po(input)], tasks=[task_create_story(input)], memory=True, verbose=True)
